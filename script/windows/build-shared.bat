4422ebd07fbc348dcac00e8cc785ec1922275f25
JaderoChan <c_dl_cn@outlook.com>
2025-07-04 19:15:42 +0800

    update

--- script/windows/build-shared.bat
+++ script/windows/build-shared.bat
@@ -3,9 +3,9 @@
 set "current_dir=%cd%"
 
 cd ../../
-cmake -B ./build/shared -DCMAKE_BUILD_TYPE=Debug -DGBHK_BUILD_SHARED=ON -DGBHK_BUILD_EXAMPLE=OFF
+cmake -B ./build/shared/debug -DCMAKE_BUILD_TYPE=Debug -DGBHK_BUILD_SHARED=ON -DGBHK_BUILD_EXAMPLE=OFF
 make -C ./build/shared -j
-cmake -B ./build/shared -DCMAKE_BUILD_TYPE=Release -DGBHK_BUILD_SHARED=ON -DGBHK_BUILD_EXAMPLE=OFF
+cmake -B ./build/shared/release -DCMAKE_BUILD_TYPE=Release -DGBHK_BUILD_SHARED=ON -DGBHK_BUILD_EXAMPLE=OFF
 make -C ./build/shared -j
 
 cd %current_dir%
