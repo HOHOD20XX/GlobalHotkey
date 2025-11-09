7fd2a3e990b59f35c534d6a2fab43ecbc238875d
JaderoChan <c_dl_cn@outlook.com>
2025-11-09 22:04:49 +0800

    update

--- script/windows/build-example.bat
+++ script/windows/build-example.bat
@@ -3,9 +3,9 @@
 set "current_dir=%cd%"
 
 cd ../../
-cmake -B ./build/example_register -DCMAKE_BUILD_TYPE=Release -DGBHK_DISABLE_HOOK=ON -DGBHK_BUILD_EXAMPLE=ON
-make -C ./build/example_register -j
-cmake -B ./build/example_hook -DCMAKE_BUILD_TYPE=Release -DGBHK_DISABLE_REGISTER=ON -DGBHK_BUILD_EXAMPLE=ON -DGBHK_EXAMPLE_USE_HOOK=ON
-make -C ./build/example_hook -j
+cmake -B ./build/example_use_register -DCMAKE_BUILD_TYPE=Release -DGBHK_DISABLE_HOOK=ON -DGBHK_BUILD_EXAMPLE=ON
+make -C ./build/example_use_register -j
+cmake -B ./build/example_use_hook -DCMAKE_BUILD_TYPE=Release -DGBHK_DISABLE_REGISTER=ON -DGBHK_BUILD_EXAMPLE=ON -DGBHK_EXAMPLE_USE_HOOK=ON
+make -C ./build/example_use_hook -j
 
 cd %current_dir%
